600619147685d1930073a937eb26356e2faf825b
Spikes-2212 <spikes.no2212@gmail.com>
2014-01-13 16:27:20 +0200

    dzn

--- GitTest/src/drivetrain/Gearbox.java
+++ GitTest/src/drivetrain/Gearbox.java
@@ -6,8 +6,8 @@ public class Gearbox {
     
     private Jaguar j1, j2;
     
-    public Gearbox(Jaguar j1, Jaguar j2) {
-        this.j1 = j1;
-        this.j2 = j2;
+    public Gearbox(int channel1, int channel2) {
+        j1 = new Jaguar(channel1);
+        j2 = new Jaguar(channel2);
     }
 }
