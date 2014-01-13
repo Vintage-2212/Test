a89577dd2d041ef8e71dbab3bb37fd8f1e9f2b14
Spikes-2212 <spikes.no2212@gmail.com>
2014-01-13 16:26:09 +0200

    Added fields & constructor to Gearbox

--- GitTest/src/drivetrain/Gearbox.java
+++ GitTest/src/drivetrain/Gearbox.java
@@ -1,15 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package drivetrain;
 
-/**
- *
- * @author asaf
- */
+import edu.wpi.first.wpilibj.Jaguar;
+
 public class Gearbox {
     
+    private Jaguar j1, j2;
+    
+    public Gearbox(Jaguar j1, Jaguar j2) {
+        this.j1 = j1;
+        this.j2 = j2;
+    }
 }
